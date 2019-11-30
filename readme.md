3e829349a070b80afc5a539ef0116ab4fb869e1b
Lenovo <77ripdrive@gmail.com>
2019-11-30 22:31:45 +0300

    Sample with Spring Gradle Web MockMVC

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Sample_Web_App
+Spring Gradle Web H2database Rest API & Test with MockMVC
+
+* Based on the results of the @fpmoles course
+* You will see reservations with  Rest API ex:http://localhost:8060/api/reservations/2019-11-30
---
+++ src/test/java/com/ripdrive/reservation/tat/web/application/ReservationControllerTest.java
@@ -0,0 +1,55 @@
+package com.ripdrive.reservation.tat.web.application;
+
+import com.ripdrive.reservation.tot.business.domain.RoomReservation;
+import com.ripdrive.reservation.tot.business.service.ReservationService;
+import com.ripdrive.reservation.tot.web.application.ReservationController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(ReservationController.class)
+public class ReservationControllerTest {
+
+    @MockBean
+    private ReservationService reservationService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Test
+    public void getReservations() throws Exception{
+        Date date = DATE_FORMAT.parse("2019-11-29");
+        List<RoomReservation> mockReservationList = new ArrayList<>();
+        RoomReservation mockRoomReservation = new RoomReservation();
+        mockRoomReservation.setLastName("Last");
+        mockRoomReservation.setFirstName("First");
+        mockRoomReservation.setDate(date);
+        mockRoomReservation.setGuestId(1);
+        mockRoomReservation.setRoomNumber("J1");
+        mockRoomReservation.setRoomId(5);
+        mockRoomReservation.setRoomName("Jangle");
+        mockReservationList.add(mockRoomReservation);
+
+        given(reservationService.getRoomReservationsForDate("2019-11-29")).willReturn(mockReservationList);
+        this.mockMvc.perform(get("/reservations?date=2019-11-29")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Last, First")));
+    }
+}
